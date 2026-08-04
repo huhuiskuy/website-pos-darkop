546f57753924336478b644de2792a357a0ac9ec5
Dika <narendra.dika379@gmail.com>
2026-08-05 01:26:19 +0700

    Gracefully handle 419 token mismatch on logout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (\Illuminate\Session\TokenMismatchException $e, \Illuminate\Http\Request $request) {
+            return redirect()->route('login');
+        });
     })->create();
